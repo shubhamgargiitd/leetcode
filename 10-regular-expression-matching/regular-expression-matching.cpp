90396588e23ee802ecc66568bce9f000bb5b64af
Shubham Garg <ch1240211@iitd.ac.in>
2026-07-27 19:32:46 +0530

    Time: 3 ms (47.00%) | Memory: 9.7 MB (18.54%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
-    bool isMatch(string s, string p) {
-        int m = s.size(), n = p.size();
-        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
-        dp[0][0] = true;
+    int m, n;
+    vector<vector<int>> memo;
+    string s, p;
 
-        auto matches = [&](int i, int j) {
-            return i > 0 && (p[j - 1] == '.' || p[j - 1] == s[i - 1]);
-        };
+    bool solve(int i, int j) {
+        // Base case
+        if (j == n) return i == m;
 
-        for (int j = 1; j <= n; ++j) {
-            for (int i = 0; i <= m; ++i) {
-                if (p[j - 1] == '*') {
-                    dp[j][i] = dp[j - 2][i] || (matches(i, j - 1) && dp[j][i - 1]);
-                } else {
-                    dp[j][i] = matches(i, j) && dp[j - 1][i - 1];
-                }
-            }
+        // Cache check
+        if (memo[i][j] != -1) return memo[i][j];
+
+        // Computation
+        bool firstMatch = (i < m) && (p[j] == s[i] || p[j] == '.');
+
+        bool ans;
+        if (j + 1 < n && p[j + 1] == '*') {
+            ans = solve(i, j + 2) || (firstMatch && solve(i + 1, j));
+        } else {
+            ans = firstMatch && solve(i + 1, j + 1);
         }
 
-        return dp[n][m];
+        return memo[i][j] = ans;
+    }
+
+    bool isMatch(string s, string p) {
+        this->s = s;
+        this->p = p;
+        m = s.size();
+        n = p.size();
+        memo.assign(m + 1, vector<int>(n + 1, -1));
+        return solve(0, 0);
     }
 };
